9cc87f465b7784aba967e73ba80d21e0a1e03cbe
Breno Santos <angusbreno@hotmail.com>
2018-08-08 09:55:46 -0300

    preparing build

--- Source/Naylah.Identity.Client/Constants/ServiceSettingsKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Naylah.Identity.Client.Constants
-{
-    public class ServiceSettingsKeys
-    {
-        public const string NaylahIdentityClientId = "NaylahIdentityClientId";
-        public const string NaylahIdentityClientSecret = "NaylahIdentityClientSecret";
-        public const string NaylahIdentityRequestScopes = "NaylahIdentityRequestScopes";
-    }
-}
--- Source/Naylah.Xamarin.Controls/Entrys/FloatLabeledEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Naylah.Xamarin.Controls.Entrys
-{
-    [Obsolete("Use new NyEntry insted with FloatLabeled to true...")]
-    public class FloatLabeledEntry : NyEntry
-    {
-    }
-}
