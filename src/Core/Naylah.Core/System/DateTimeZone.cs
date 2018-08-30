52bc20a5c673a96a75ab36319862cee599205b24
Breno Santos <angusbreno@hotmail.com>
2018-08-30 15:33:57 -0300

    DateTimeZone

---
+++ src/Core/Naylah.Core/System/DateTimeZone.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Naylah.System
+{
+    public class DateTimeZone : IEquatable<DateTimeZone>, IEquatable<DateTime>
+    {
+        private const string defaultTimeZoneInfoId = "UTC";
+
+        public DateTimeZone(DateTime dateTime, string timeZoneInfoId = defaultTimeZoneInfoId)
+        {
+            if (timeZoneInfoId == defaultTimeZoneInfoId)
+            {
+                DateTime = new DateTime(dateTime.Ticks, DateTimeKind.Utc);
+            }
+            else
+            {
+                DateTime = new DateTime(dateTime.Ticks, DateTimeKind.Unspecified);
+            }
+
+            TimeZoneInfoId = timeZoneInfoId;
+        }
+
+        public DateTimeZone()
+        {
+            TimeZoneInfoId = defaultTimeZoneInfoId;
+        }
+
+        //[JsonProperty]
+        public DateTime DateTime { get; protected set; }
+
+        public string TimeZoneInfoId { get; protected set; }
+
+        public TimeZoneInfo TimeZoneInfo { get { return TimeZoneInfo.FindSystemTimeZoneById(TimeZoneInfoId); } }
+
+        #region Cast operators
+
+        public static implicit operator DateTimeZone(DateTime dateTime)
+        {
+            return new DateTimeZone(dateTime);
+        }
+
+        public static implicit operator DateTime(DateTimeZone dateTimeWithZone)
+        {
+            return dateTimeWithZone.DateTime;
+        }
+
+        #endregion Cast operators
+
+        #region Condition operators, IEqualitable
+
+        public static bool operator ==(DateTimeZone left,
+                                     DateTimeZone right)
+        {
+            if (Object.ReferenceEquals(left, null)) return false;
+            else return left.Equals(right);
+        }
+
+        public static bool operator !=(DateTimeZone left,
+                                        DateTimeZone right)
+        {
+            return !(left == right);
+        }
+
+        public bool Equals(DateTimeZone other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.DateTime, DateTime) && other.TimeZoneInfoId == TimeZoneInfoId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is DateTimeZone)) return false;
+            return Equals((DateTimeZone)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 397;
+            int result = DateTime.GetHashCode();
+            result = (result * prime) ^ (TimeZoneInfoId != null ? TimeZoneInfoId.GetHashCode() : 0);
+            return result;
+        }
+
+        #region DateTime comparation
+
+        public static bool operator ==(DateTimeZone left,
+                                    DateTime right)
+        {
+            if (Object.ReferenceEquals(left, null)) return false;
+            else return left.Equals(right);
+        }
+
+        public static bool operator !=(DateTimeZone left,
+                                        DateTime right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(DateTime left,
+                                    DateTimeZone right)
+        {
+            if (Object.ReferenceEquals(left, null)) return false;
+            else return right.Equals(left);
+        }
+
+        public static bool operator !=(DateTime left,
+                                        DateTimeZone right)
+        {
+            return !(left == right);
+        }
+
+        public bool Equals(DateTime other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other, DateTime);
+        }
+
+        #endregion DateTime comparation
+
+        #endregion Condition operators, IEqualitable
+    }
+}
---
+++ src/Core/Naylah.Core/System/DateTimeZonePeriod.cs
@@ -0,0 +1,92 @@
+﻿using Naylah.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace Naylah.System
+{
+    public static class DateTimeZonePeriodExtensions
+    {
+        public static void ConvertToTimeZone(this DateTimeZonePeriod durationTimeBox, string timeZoneInfoId)
+        {
+            var timeZoneInfoTo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneInfoId);
+
+            durationTimeBox.Start = new DateTimeZone(
+                TimeZoneInfo.ConvertTime(durationTimeBox.Start.DateTime, durationTimeBox.Start.TimeZoneInfo, timeZoneInfoTo), timeZoneInfoId
+                );
+            durationTimeBox.End = new DateTimeZone(
+                TimeZoneInfo.ConvertTime(durationTimeBox.End.DateTime, durationTimeBox.End.TimeZoneInfo, timeZoneInfoTo), timeZoneInfoId
+                );
+        }
+
+        public static void ConvertToTimeZone(this IEnumerable<DateTimeZonePeriod> durationTimeBoxes, string timeZoneInfoId)
+        {
+            durationTimeBoxes.ForEach(x => x.ConvertToTimeZone(timeZoneInfoId));
+        }
+    }
+
+    public class DateTimeZonePeriod : IEquatable<DateTimeZonePeriod>
+    {
+        public DateTimeZonePeriod()
+        {
+            Start = new DateTimeZone();
+            End = new DateTimeZone();
+        }
+
+        public DateTimeZone Start { get; set; }
+        public DateTimeZone End { get; set; }
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                try
+                {
+                    return (End.DateTime - Start.DateTime).Duration();
+                }
+                catch (Exception)
+                {
+                    return TimeSpan.Zero;
+                }
+            }
+        }
+
+        #region Condition operators, IEqualitable
+
+        public static bool operator ==(DateTimeZonePeriod left,
+                                     DateTimeZonePeriod right)
+        {
+            if (Object.ReferenceEquals(left, null)) return false;
+            else return left.Equals(right);
+        }
+
+        public static bool operator !=(DateTimeZonePeriod left,
+                                        DateTimeZonePeriod right)
+        {
+            return !(left == right);
+        }
+
+        public bool Equals(DateTimeZonePeriod other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.Start, Start) && other.End == End;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is DateTimeZonePeriod)) return false;
+            return Equals((DateTimeZonePeriod)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 397;
+            int result = Start.GetHashCode() ^ End.GetHashCode();
+            result = (result * prime) ^ (Start != null ? Start.GetHashCode() : 0);
+            result = (result * prime) ^ (End != null ? End.GetHashCode() : 0);
+            return result;
+        }
+
+        #endregion Condition operators, IEqualitable
+    }
+}
